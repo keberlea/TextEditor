e5e49b53c6e752f54d4ae74779d9a78c994d7667
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-06-11 22:09:01 -0700

    added to database.js, logic to method that accepts some conent adds to db  and gets content from db

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,38 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+const addContent = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+  await tx.done;
+};
+
+export const putDb = async (content) => console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+const getContent = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.getAll();
+  await tx.done;
+  return content;
+};
+
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
